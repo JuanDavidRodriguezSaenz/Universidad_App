dc157bf6b2238614c9b9cdcdb373348141b4888d
FlutLab.io <>
2022-05-03 14:28:49 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rodriguez/galeriadeimagenes.dart';
 
 void main() {
   runApp(MiEscuelaParticularApp());
@@ -16,45 +17,3 @@ class MiEscuelaParticularApp extends StatelessWidget {
         home: PaginaInicial());
   } // widget
 } // clase prepa particular app widget sin estado
-
-class PaginaInicial extends StatefulWidget {
-  PaginaInicial({Key? key}) : super(key: key);
-  @override
-  _PaginaInicialState createState() => _PaginaInicialState();
-} // widget con estado
-
-class _PaginaInicialState extends State<PaginaInicial> {
-  List<String> images = [
-    "assets/images/alumnos1.jpg",
-    "assets/images/escuela1.jpg",
-    "assets/images/maestro1.jpg",
-    "assets/images/maestro2.jpg",
-    "assets/images/maestro3.jpg",
-    "assets/images/libro1.jpg",
-    "assets/images/director1.jpg",
-    "assets/images/director2.jpg",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Tutorial Grid View"),
-        ),
-        body: GridView.custom(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-            (BuildContext, index) {
-              return Image.asset(
-                images[index],
-                fit: BoxFit.cover,
-              );
-            },
-            childCount: 8,
-          ),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
